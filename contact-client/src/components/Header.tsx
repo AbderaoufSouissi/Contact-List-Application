bb78177694cc851dbd2c2c068e5b557363e37d18
abderaouf souissi <abderaouf.souissi@outlook.com>
2025-03-29 12:49:00 +0100

    frontend: add Header component

---
+++ contact-client/src/components/Header.tsx
@@ -0,0 +1,19 @@
+type HeaderProps = {
+  toggleModal: (parm: boolean) => void;
+  nbOfContacts: number;
+};
+
+const Header = ({ toggleModal, nbOfContacts }: HeaderProps) => {
+  return (
+    <header className="header">
+      <div className="container">
+        <h3>Contact List ({nbOfContacts})</h3>
+        <button className="btn" onClick={() => toggleModal(true)}>
+          Add New Contact <i className="bi bi-plus-square"></i>
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
