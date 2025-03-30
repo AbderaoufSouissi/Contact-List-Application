d128e9addceedfd8860f6e147b683c2d9d8f497a
abderaouf souissi <abderaouf.souissi@outlook.com>
2025-03-30 22:38:40 +0200

    backend: fix image upload functionnality

--- contact-api/src/main/java/com/ars/contact_api/service/ContactService.java
+++ contact-api/src/main/java/com/ars/contact_api/service/ContactService.java
@@ -71,13 +71,13 @@ public class ContactService {
         String filename = id + fileExtension.apply(image.getOriginalFilename());
         try {
             Path fileStorageLocation = Paths.get(PHOTO_DIRECTORY).toAbsolutePath().normalize();
-            if(!Files.exists(fileStorageLocation)) {
-                Files.createDirectory(fileStorageLocation);
-            }
-            Files.copy(image.getInputStream(),fileStorageLocation.resolve(filename), REPLACE_EXISTING);
-            return ServletUriComponentsBuilder.fromCurrentContextPath().path("/contacts/image/" + fileExtension.apply(image.getOriginalFilename())).toUriString();
+            if(!Files.exists(fileStorageLocation)) { Files.createDirectories(fileStorageLocation); }
+            Files.copy(image.getInputStream(), fileStorageLocation.resolve(filename), REPLACE_EXISTING);
+            return ServletUriComponentsBuilder
+                    .fromCurrentContextPath()
+                    .path("/contacts/image/" + filename).toUriString();
         }catch (Exception exception) {
-            throw new ApiException("Unable to save image");
+            throw new RuntimeException("Unable to save image");
         }
     };
 
